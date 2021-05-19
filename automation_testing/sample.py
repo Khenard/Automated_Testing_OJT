f92558e9ceeebcbaec3a7418aa74cf63308e7c91
Khenard Figuracion <khenard.figuracion@gmail.com>
2021-05-19 13:42:01 +0800

    sample

--- automation_testing/sample.py
+++ automation_testing/sample.py
@@ -1,5 +1,11 @@
-'''
-Created on May 19, 2021
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import os
 
-@author: Ken
-'''
+#Configure web driver
+chromedriver = os.path.abspath("chromedriver.exe")
+driver = webdriver.Chrome(chromedriver)
+driver.maximize_window()
+
+driver.get("https://qado.medisource.com/patient")
