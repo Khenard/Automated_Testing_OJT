5b6102e3078066c5cd4073d51f87ea84b049b225
Khenard Figuracion <khenard.figuracion@gmail.com>
2021-05-21 14:32:14 +0800

    sample

---
+++ automation_testing/function.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import os
+import time
+import config
+
+
+
+
+def loginfunction():
+    
+    time.sleep(5)
+    usernametb = config.driver.find_element_by_id('loginemail').send_keys('superagent@unitest')
+    time.sleep(5)
+    passwordtb = config.driver.find_element_by_id('loginpassword')
+    passwordtb.send_keys('Tester2021@')
+    time.sleep(3)
+    loginbtn = config.driver.find_element_by_xpath('//*[@id="mhLP-ln"]/div[2]/form/div[6]/button')
+    loginbtn.click() 
--- automation_testing/sample.py
+++ automation_testing/sample.py
@@ -2,10 +2,19 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 import os
+import time
+import function
+import config
 
-#Configure web driver
-chromedriver = os.path.abspath("chromedriver.exe")
-driver = webdriver.Chrome(chromedriver)
-driver.maximize_window()
 
-driver.get("https://qado.medisource.com/patient")
+config.driver.get("https://qado.medisource.com/login")
+config.driver.maximize_window()
+
+function.loginfunction()
+
+time.sleep(5)
+
+config.driver.get('https://qado.medisource.com/patients/admitted')
+time.sleep(3)
+searchtb = config.driver.find_element_by_css_selector('#searchbar__wrapper > div > input').send_keys('automated')
+
