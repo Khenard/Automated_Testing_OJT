f81519adcace987651dea6217999f771297dc54b
Khenard Figuracion <khenard.figuracion@gmail.com>
2021-05-21 14:43:30 +0800

    sample

--- automation_testing/function.py
+++ automation_testing/function.py
@@ -6,15 +6,13 @@ import time
 import config
 
 
-
-
-def loginfunction():
+def loginfunction(un, up):
     
     time.sleep(5)
-    usernametb = config.driver.find_element_by_id('loginemail').send_keys('superagent@unitest')
+    usernametb = config.driver.find_element_by_id('loginemail').send_keys(un)
     time.sleep(5)
     passwordtb = config.driver.find_element_by_id('loginpassword')
-    passwordtb.send_keys('Tester2021@')
+    passwordtb.send_keys(up)
     time.sleep(3)
     loginbtn = config.driver.find_element_by_xpath('//*[@id="mhLP-ln"]/div[2]/form/div[6]/button')
     loginbtn.click() 
--- automation_testing/sample.py
+++ automation_testing/sample.py
@@ -10,11 +10,22 @@ import config
 config.driver.get("https://qado.medisource.com/login")
 config.driver.maximize_window()
 
-function.loginfunction()
+function.loginfunction('superagent@unitest', 'Tester2021@')
 
 time.sleep(5)
 
 config.driver.get('https://qado.medisource.com/patients/admitted')
 time.sleep(3)
-searchtb = config.driver.find_element_by_css_selector('#searchbar__wrapper > div > input').send_keys('automated')
+
+
+
+def searchpatient():
+    searchtb = config.driver.find_element_by_css_selector('#searchbar__wrapper > div > input').send_keys('automated')
+
+
+searchpatient()
+
+
+
+
 
